e3a4f07fcd8a74e5fd95456a6c7a136493aa14b0
Алекса Ћетковић <cetkovic.sv77.2022@uns.ac.rs>
2025-10-07 15:59:50 +0200

    add temperature sensor service contract

---
+++ KelvinConsensus/KelvinConsensus/TemperatureSensor.svc.cs
@@ -0,0 +1,11 @@
+using System;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace KelvinConsensus
+{
+    public class TemperatureSensor : ITemperatureSensor
+    {
+        // TODO: Implement temperature sensor service
+    }
+}
